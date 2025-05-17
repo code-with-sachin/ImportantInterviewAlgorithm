853da7eb304ef3dfaf1ed0dc21e8efc3c16b1501
Sachin <sachinkaushikofficial@gmail.com>
2025-05-17 17:29:36 +0530

    Optimizing the code to reduce TC

--- src/leetcode/ContainsDuplicates.java
+++ src/leetcode/ContainsDuplicates.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class ContainsDuplicates {
 
     public static void main(String[] args) {
@@ -7,18 +9,34 @@ public class ContainsDuplicates {
         System.out.println(hasDuplicate(nums));
     }
 
-    public static boolean hasDuplicate(int[] nums) {
-        if(nums.length<=1){
-            return false;
-        }
-        //brute force
-        for(int i=0; i<nums.length-1; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i]==nums[j]){
+    //Sorting technique
+        public static boolean hasDuplicate(int[] nums) {
+            if(nums.length<=1){
+                return false;
+            }
+            Arrays.sort(nums);
+            //linear search
+            for(int i=0; i<nums.length-1; i++){
+                if(nums[i]==nums[i+1]){
                     return true;
                 }
             }
+            return false;
         }
-        return false;
-    }
+
+    //brute force
+//    public static boolean hasDuplicate(int[] nums) {
+//        if(nums.length<=1){
+//            return false;
+//        }
+//        //brute force
+//        for(int i=0; i<nums.length-1; i++){
+//            for(int j=i+1; j<nums.length; j++){
+//                if(nums[i]==nums[j]){
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
 }
